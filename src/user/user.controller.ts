11c14aeb8d084c3b2ec7a032514927ff5549a50a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-12 00:47:43 -0100

    created endpoints for our user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { Body, Delete, Get, Param, Put } from '@nestjs/common/decorators';
+import { Observable } from 'rxjs';
+import { User } from './model/user-interface';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() id: number): Observable<User> {
+    return this.userService.deleteOne(id);
+  }
+
+  @Get()
+  findAllUsers(): Observable<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOneUser(@Param() id: number): Observable<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  updateUser(@Param() id: number, @Body() user: User): Observable<User> {
+    return this.userService.updateOne(id, user);
+  }
+}
