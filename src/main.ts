c0699589b3d6f210c7f01bf6319313da2746954a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-14 19:11:25 -0100

    added cors for localhost 4200

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
